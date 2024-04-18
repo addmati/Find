aaa021357565d7981d695882a396d85e44879205
addmati <audtn0409@naver.com>
2024-04-18 22:25:06 +0900

    Inlt

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,84 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Card firstCard;
+    public Card secondCard;
+
+
+
+    public Text timeTxt;
+    public GameObject endTxt;
+
+
+    public int cardCount = 0;
+
+    float time = 0.0f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public AudioClip clip;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+
+        if (time > 30.0f)
+        {
+            endTxt.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void Matched()
+    {
+        if (firstCard.idx == secondCard.idx)
+        {
+
+            audioSource.PlayOneShot(clip);
+
+
+            firstCard.DestoryCard();
+            secondCard.DestoryCard();
+
+            cardCount -= 2;
+            if (cardCount == 0)
+            {
+                Time.timeScale = 0.0f;
+                endTxt.SetActive(true);
+            }
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+
+
+
+}
